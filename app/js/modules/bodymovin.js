ae279f94883681695f154e5b876ea2cab24017eb
Ihor <icassidyif@gmail.com>
2020-10-14 15:49:24 +0300

    burger fix

--- app/js/modules/bodymovin.js
+++ app/js/modules/bodymovin.js
@@ -12,6 +12,7 @@ let burgerAnimation = lottie.loadAnimation({
   path: 'assets/burger.json'
 });
 
+burgerAnimation.setSpeed(1.1);
 
 let burgerOpen = false;
 let animationClickInProgress = false;
@@ -20,27 +21,28 @@ hoverBurger();
 clickBurger();
 
 function hoverBurger() {
-  if(burgerOpen) {
-    $(burgerContainer).unbind('mouseenter mouseleave');
-  } else {
-    $(burgerContainer).hover(
-      // hover in
-      () => {
-        if(animationClickInProgress) {
-          burgerAnimation.onComplete = () => {animationClickInProgress = false;}
-        } else {
-          hoverIn();
-        }
-      },
-      //hover out
-      () => {
-        if(animationClickInProgress) {
-          burgerAnimation.onComplete = () => {animationClickInProgress = false; hoverOut(); hoverBurger();}
-        } else {
+  $(burgerContainer).unbind('mouseenter mouseleave');
+  $(burgerContainer).hover(
+    // hover in
+    () => {
+      if(animationClickInProgress) {
+        burgerAnimation.onComplete = () => {animationClickInProgress = false;}
+      } else {
+        hoverIn();
+      }
+    },
+    //hover out
+    () => {
+      if(animationClickInProgress) {
+        burgerAnimation.onComplete = () => {
+          animationClickInProgress = false;
           hoverOut();
+          hoverBurger();
         }
-      })
-  }
+      } else {
+        hoverOut();
+      }
+    })
 }
 
 function clickBurger() {
@@ -55,18 +57,21 @@ function clickBurger() {
     }else {
       $(burgerContainer).unbind('mouseenter mouseleave');
       burgerAnimation.playSegments([10, 20], true);
+      animationClickInProgress = true;
+      hoverBurger();
+      burgerAnimation.onComplete = () => {animationClickInProgress = false; hoverBurger();}
     }
   })
 }
 
 function hoverIn() {
-  burgerAnimation.playSegments([0, 10], true);
+  burgerOpen? burgerAnimation.playSegments([20, 30], true) : burgerAnimation.playSegments([0, 10], true);
   burgerAnimation.onComplete = () => {};
 }
 function hoverOut() {
-  burgerAnimation.playSegments([10, 0], true);
+  burgerOpen? burgerAnimation.playSegments([30, 20], true) : burgerAnimation.playSegments([10, 0], true);
   burgerAnimation.onComplete = () => {};
 }
+//End Burger animation
 
 
-export {burgerAnimation};
